0a53252b8b2c68255da3abee4da6d1ee6c8dbfd9
goku007xx <gokulkarthik12345@gmail.com>
2021-11-20 08:49:14 -0500

    New code

--- Car_rental/employee_access/urls.py
+++ Car_rental/employee_access/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django.conf.urls import include, url
 
-from customer_access.views import authentication
-from customer_access.views import customer_view
+from employee_access.views import authentication
+from employee_access.views import employee_view
 
 urlpatterns = [
-    path('signin/', authentication.signin, name='signin')
-    #path('logout/', authentication.logout, name='logout'),
+    path('signin/', authentication.signin, name='signin'),
+    path('home/', employee_view.home, name = 'home'),
+    path('logout/', authentication.logout, name='logout'),
+    path('view_res/' , employee_view.view_res , name = 'view_res'),
+    path('cancel_reservation/', employee_view.cancel_reservation, name='cancel_reservation'),
     #path('vehicle/' , customer_view.vehicle, name = 'vehicle')
 ]
--- Car_rental/employee_access/views.py
+++ Car_rental/employee_access/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.shortcuts import redirect
+import json
+import psycopg2
+import random
 
 class authentication():
 
@@ -11,10 +17,12 @@ class authentication():
         if request.method == 'GET':
             if request.session.has_key('emp_phoneno'):
 
-                #phoneno = request.session['phoneno']
-                #context = {'phoneno' : phoneno}
-                #return render(request,'home.html',context)
-                return HttpResponse("Signed in :)")
+                phoneno = request.session['emp_phoneno']
+                context = {'phoneno' : phoneno}
+                #return render(request,'ehome2.html',context)
+                resp = redirect('/employee/home/')
+                return resp
+                #return HttpResponse("Signed in :)")
 
             else:
                 return render(request,'esignin.html')
@@ -43,8 +51,8 @@ class authentication():
                     conn.close()
                     #request.session.set_expiry(300)
                     request.session['emp_phoneno'] = phoneno
-                    #response = redirect('/home/')
-                    return HttpResponse("123WORKS")
+                    response = redirect('/employee/home/')
+                    return response
     
                 cur.close()
                 conn.close()
@@ -56,4 +64,133 @@ class authentication():
 
         else:
             return HttpResponse("METHOD NOT ALLOWED")
+
+    def logout(request):
+        try:
+            del request.session['emp_phoneno']
+            return HttpResponse("<strong>Employee Logged out Successfully.</strong>")
+        except:
+            print("Logout not Work!!!")
+            return HttpResponse("<strong>Failure in Logging out.</strong>")
 # Create your views here.
+
+class employee_view():
+    
+    def home(request):
+        if request.method == 'GET':
+            if request.session.has_key('emp_phoneno'):
+
+                try:
+                    conn = psycopg2.connect("dbname='trial_db' user='postgres' host='localhost' password='trial@123'")
+                    cur = conn.cursor()
+                except:
+                    print("Unable to connect to the database")
+                    return HttpResponse("Unable to connect to the database")
+                
+                phoneno = request.session['emp_phoneno']
+
+                context={"phoneno":phoneno}
+                        #"reservations":rows
+                    #request.session.set_expiry(300)
+                
+                return render(request, 'ehome.html', context)
+                #phoneno = request.session['emp_phoneno']
+                #context = {'phoneno' : phoneno}
+
+                #return render(request,'ehome.html',context)
+            else:
+                #context = {'error' : 'Sign In Please'}
+                return redirect('/employee/signin/')
+        else:
+            return HttpResponse('METHOD NOT ALLOWED')
+
+    @csrf_exempt
+    def view_res(request):
+        if request.method =='GET':
+            if request.session.has_key('emp_phoneno'):
+                try:
+                    conn = psycopg2.connect("dbname='trial_db' user='postgres' host='localhost' password='trial@123'")
+                    cur = conn.cursor()
+                except:
+                    print("Unable to connect to the database")
+                    return HttpResponse("Unable to connect to the database")
+
+            phoneno = request.session['emp_phoneno']
+
+            query = f"SELECT employee_id,employee_name FROM employee where employee_phone_no ='{phoneno}'"
+            #print(query)
+            cur.execute(query)
+            row = cur.fetchone()
+
+            emp_id = row[0]
+
+                
+            print(row)
+            query = f"SELECT reservation_date,vehicle_taken_date,expected_return_date,plt_no_id,customer_id,reservation_id from reservation where emp_id = '{emp_id}' and reservation_status='inprogress'"
+            cur.execute(query)
+            rows = cur.fetchall()
+            print(rows)
+
+            context = {'phoneno' : phoneno , 'reservations' : rows}
+
+            return render(request,'view_res.html',context = context)
+
+    @csrf_exempt
+    def cancel_reservation(request):
+        if request.method =='POST':
+            if request.session.has_key('emp_phoneno'):
+                try:
+                    conn = psycopg2.connect("dbname='trial_db' user='postgres' host='localhost' password='trial@123'")
+                    cur = conn.cursor()
+                except:
+                    print("Unable to connect to the database")
+                    return HttpResponse("Unable to connect to the database")
+
+                body = json.loads(request.body)
+                res_id = body['id']
+                print(res_id)
+
+                query = f"select plt_no_id from reservation where reservation_id='{res_id}'"
+                cur.execute(query)
+                row = cur.fetchone()
+                plate_no = row[0]
+                print(plate_no)
+                print("Here")
+
+                query_update = f"update vehicle set vehicle_status='not-taken' where plate_number='{plate_no}';"
+                cur.execute(query_update)
+                conn.commit()
+
+                print("Here2")
+
+                query_delete = f"delete from reservation where reservation_id='{res_id}';"
+                cur.execute(query_delete)
+                conn.commit()
+
+                print("Here3")
+                phoneno = request.session['emp_phoneno']
+
+                query = f"SELECT employee_id,employee_name FROM employee where employee_phone_no ='{phoneno}'"
+                    #print(query)
+                cur.execute(query)
+                row = cur.fetchone()
+
+                emp_id = row[0]
+
+
+                query = f"SELECT reservation_date,vehicle_taken_date,expected_return_date,plt_no_id,customer_id,reservation_id from reservation where emp_id = '{emp_id}' and reservation_status='inprogress'"
+                cur.execute(query)
+                rows = cur.fetchall()
+
+                context = {'phoneno' : phoneno , 'reservations' : rows}
+                cur.close()
+                conn.close()
+                print("Here4")
+
+                return HttpResponse("Works1")
+                #return redirect('/employee/home/')
+
+            else:
+                return redirect('/employee/signin/')
+        else:
+            return HttpResponse("METHOD NOT ALLOWED")
