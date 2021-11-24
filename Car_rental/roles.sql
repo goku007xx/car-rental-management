5fecff634faaf55fb3870894a9e7cf3826ee621e
Gaurav J <gauravjayaraj@gmail.com>
2021-11-24 17:46:38 +0530

    Roles file and small style

---
+++ Car_rental/roles.sql
@@ -0,0 +1,26 @@
+\c trial_db
+-- CUSTOMER:
+create role customer_role;
+grant select,insert on customer to customer_role;
+grant select,update on vehicle to customer_role;
+grant select,insert on reservation to customer_role;
+grant select on outlet to customer_role;
+grant select on employee to customer_role ;
+create user customer with password '123';
+grant customer_role to customer;
+
+-- EMPLOYEE:
+create role employee_role;
+grant select on employee to employee_role;
+grant select on customer to employee_role;
+grant select,update on reservation to employee_role;
+grant select,insert,update on rent to employee_role;
+grant select,insert,update,delete on reservation to employee_role;
+grant select,update on vehicle to employee_role;
+grant select,update on outlet to employee_role;
+create user employee with password '456';
+grant employee_role to employee;
+
+-- DJANGO STUFF:
+grant select,update on outlet,customer_customer_id_seq ,employee_employee_id_seq,employee_employee_id_seq,outlet_outlet_id_seq,rent_bill_id_seq to employee_role;
+grant select,update on outlet,customer_customer_id_seq ,employee_employee_id_seq,employee_employee_id_seq,outlet_outlet_id_seq,rent_bill_id_seq,reservation_reservation_id_seq to customer_role;
