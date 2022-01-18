bd9d170eb490375b90db43c422e3043f25a11d7b
goku007xx <gokulkarthik12345@gmail.com>
2022-01-18 12:04:25 -0500

    Removed phoneno module in settings.py

--- Car_rental/Car_rental/settings.py
+++ Car_rental/Car_rental/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'phonenumber_field',
     'django.contrib.staticfiles',
 ]
 
