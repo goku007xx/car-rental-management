45b748e59fbaa30db0b84607541aa315f25c3454
goku007xx <gokulkarthik12345@gmail.com>
2022-01-19 06:52:39 -0500

    Changes to run.sh

--- Car_rental/run.sh
+++ Car_rental/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo apt install libpq-dev
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'trial@123'"
 sudo -u postgres psql -c "CREATE DATABASE trial_db"
+pip3 install Django
+pip3 install psycopg2
+python3 manage.py makemigrations
+python3 manage.py migrate
 sudo -u postgres psql -d trial_db -f roles.sql
 sudo -u postgres psql -d trial_db -f dml.sql
